f7ca7a1963f715606f82add6f986282bb57008a7
D.M <578328962@qq.com>
2024-09-29 18:56:46 +0800

    feat: 添加新方法

--- src/Common/Response.php
+++ src/Common/Response.php
@@ -11,15 +11,38 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 class Response
 {
 
+    /**
+     * 返回一个标准的 JSON 响应
+     *
+     * @static
+     * @param mixed $data 要返回的数据
+     * @param string $message 响应信息，默认是 '请求成功'
+     * @param int $code HTTP 状态码，默认是 200
+     * @param array $header 额外的 HTTP 头
+     * @return JsonResponse
+     */
     static public function respond($data = null, $message = '请求成功', $code = JsonResponse::HTTP_OK, array $header = [])
     {
-        return response()->json([
+        $res = $data === false ? [
+            'code' => $code,
+            'message' => $message
+        ] : [
             'code' => $code,
             'message' => $message,
             'data' => $data ? $data : null
-        ], $code, $header, JSON_UNESCAPED_UNICODE);
+        ];
+
+        return response()->json($res, $code, $header, JSON_UNESCAPED_UNICODE);
     }
 
+
+    /**
+     * 处理异常并返回标准化的 JSON 响应
+     *
+     * @static
+     * @param mixed $exceptions 要处理的异常
+     * @return void
+     */
     static public function exception($exceptions)
     {
         $exceptions->render(function (Exception $e) {
@@ -27,7 +50,7 @@ class Response
             else if ($e instanceof AuthenticationException) $code = JsonResponse::HTTP_UNAUTHORIZED;
             else $code = $e->getCode() ? $e->getCode() : JsonResponse::HTTP_INTERNAL_SERVER_ERROR;
 
-            return self::respond(null, $e->getMessage(), $code);
+            return self::respond(false, $e->getMessage(), $code);
         });
     }
 }
